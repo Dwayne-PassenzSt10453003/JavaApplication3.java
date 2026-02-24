1786fd3753875290ea1020a7284b9e224ae8d759
Dwayne Passenz <ST10453003@vcconnect.edu.za>
2026-02-24 09:22:40 +0200

    Stored Data will be stored. Details will be displayed. This is a basic project showcasing some code.

---
+++ src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,44 @@
+//single line comment
+//This is my first java project 
+
+/*This
+is 
+a
+multi-line
+comment
+*/
+
+package javaapplication5;
+
+import java.util.Scanner;
+
+public class JavaApplication5 {
+    
+/*
+{This is a block}
+ */
+
+//This statement is a method 
+//Static is one copy of method, there is one instance
+//of the method
+//void - returns nothing 
+    public static void main(String[] args) {
+     //Create tne scanner object
+     Scanner inputOne = new Scanner(System.in);
+     //data types
+     //variables
+     //[D][w][a][y][n][e]
+    String myName = "I am Dwayne";
+     char initial = 'D';
+     int myAge = 20;
+     double  myFractionAge = 20.95;
+             
+     System.out.print("Hello" + " ! " + myName +  " ! " + initial + " ! " + myAge + " ! " + " ! " +myFractionAge);
+     System.out.println("\nPlease enter your ID:" );        
+      String ID = inputOne.next();
+      System.out.println("Hello" + myName + ",your ID is:" + ID);
+     
+      }
+    }
+    
+
